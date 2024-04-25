e5b9c3ea9f0744efcab3b508872afb31fb22c564
cadonj12 <cadonjohnson21@gmail.com>
2024-04-24 19:17:47 -0700

    question 4

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,18 @@
+#Task 1
+
+price_bread = 2.50
+price_peanut_butter = 3.25
+price_jelly = 3.75
+
+total_cost = price_bread + price_peanut_butter + price_jelly
+
+print(total_cost)
+
+#Task 2
+
+initial_amount = 10000
+interest_rate = 0.07
+
+total_amount = initial_amount * (1 + interest_rate)
+
+print(total_amount)
