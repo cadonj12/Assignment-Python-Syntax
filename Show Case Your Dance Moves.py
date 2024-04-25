c6b3f0b7a7a1f3b28f23f3fd012e9b7c97a1013f
cadonj12 <cadonjohnson21@gmail.com>
2024-04-24 18:44:29 -0700

    1st question

---
+++ Show Case Your Dance Moves.py
@@ -0,0 +1,18 @@
+#Task 1
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+#Task 2
+
+user_feeling = input("How are you feeling today?")
+
+if user_feeling == "happy":
+    print("That's great to hear!")
+if user_feeling == "sad":
+    print("I hope your day gets better!")
