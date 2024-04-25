a4682b784ab3c915f125035d6fccfb74c8a21aec
cadonj12 <cadonjohnson21@gmail.com>
2024-04-24 19:57:10 -0700

    question 6

---
+++ Exploring Logical Operations Syntax.py
@@ -0,0 +1,13 @@
+#Task 1
+
+result_without_parantheses = 6 + 8 * 4
+result_with_parantheses = (6 + 8) * 4
+
+print(result_without_parantheses)
+print(result_with_parantheses)
+
+#Task 2
+
+result = 1 < (5 + 9) or (3 + 5) > 3
+
+print(result)
